4ca0867f78df5a8bfecb4a6575a839640f84e486
Warren <wjohn60@wgu.edu>
2019-08-01 13:56:52 -0400

    Adds application views.py file and updates urls.py.

--- ordermanager/urls.py
+++ ordermanager/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from ordermanager import views
 
 urlpatterns = [
+    path('', views.OrderListView.as_view(), name='order_list'),
     path('about/', views.AboutView.as_view(), name='about'),
+    path('order/<int:pk>', views.OrderDetailView.as_view(), name='order_detail'),
+    path('order/new/', views.CreateOrderView.as_view(), name='order_new'),
+    path('order/<int:pk>/edit/', views.OrderUpdateView.as_view(), name='order_edit'),
 ]
--- ordermanager/views.py
+++ ordermanager/views.py
@@ -1,7 +1,35 @@
 from django.shortcuts import render
-from django.views.generic import (TemplateView)
+from ordermanager.models import Order, Contact
+from django.views.generic import (TemplateView, ListView, DetailView, CreateView, UpdateView)
+from django.contrib.auth.mixins import LoginRequiredMixin
+from ordermanager.forms import OrderForm
 # Create your views here.
 
 
 class AboutView(TemplateView):
     template_name = 'about.html'
+
+
+class OrderListView(ListView):
+    model = Order
+
+    def get_queryset(self):
+        return Order.objects.filter().order_by('-date')
+
+
+class OrderDetailView(DetailView):
+    model = Order
+
+
+class CreateOrderView(LoginRequiredMixin, CreateView):
+    login_url = '/login/'
+    redirect_field_name = 'ordermanager/order_detail.html'
+    form_class = OrderForm
+    model = Order
+
+
+class OrderUpdateView(LoginRequiredMixin, UpdateView):
+    login_url = '/login/'
+    redirect_field_name = 'ordermanager/order_detail.html'
+    form_class = OrderForm
+    model = Order
